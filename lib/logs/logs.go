8744c93e5a4f7dc929a7638d021107b18428ada2
alexshe <alexshe@wix.com>
2017-07-13 21:02:42 +0300

    logs

--- lib/logs/logs.go
+++ lib/logs/logs.go
@@ -1,1 +1,59 @@
 package logs
+
+import (
+"github.com/Sirupsen/logrus"
+"os"
+
+"github.com/logrusorgru/aurora"
+)
+
+
+var OsGetenv = os.Getenv
+
+const colorsFlag bool = true
+
+type Logger struct {
+	internalLog *logrus.Logger
+	prefix string
+	constFileds map[string]interface{}
+	CS aurora.Aurora
+}
+
+
+func NewLogger(name string) *Logger {
+	obj := new (Logger)
+	obj.CS = aurora.NewAurora(colorsFlag)
+	obj.internalLog = logrus.New()
+	obj.internalLog.Level = logrus.DebugLevel
+	if ( os.Getenv("C3LOGFMT") == "JSON" ) {
+		obj.internalLog.Formatter = &logrus.JSONFormatter{}
+	}
+	obj.prefix = name
+	return obj
+}
+
+func (obj Logger)SubLogger (name string) *Logger{
+	retval := NewLogger(obj.prefix + name)
+	retval.constFileds = obj.constFileds
+
+	return retval
+}
+
+func (obj Logger) Info(msg string, fields logrus.Fields){
+	for key, value := range obj.constFileds {
+		fields[key] = value
+	}
+	obj.internalLog.WithFields( fields ).Debug( obj.CS.Sprintf( obj.CS.Cyan("[ %s ] %s"), obj.CS.Magenta( obj.prefix ) , msg ) )
+}
+
+func (obj Logger)  Log(msg string ){
+	obj.internalLog.Info( obj.CS.Sprintf("[ %s ] %s", obj.CS.Green( obj.prefix ) , msg )  )
+}
+
+func (obj Logger)  Debug(msg string ){
+	obj.internalLog.Debug( obj.CS.Sprintf(obj.CS.Cyan ( "[ %s ] %s" ), obj.CS.Green( obj.prefix ) , msg ) )
+}
+
+func (obj Logger)  Error(err error ){
+	obj.internalLog.Error( obj.CS.Sprintf( "[ %s ] %s" , obj.CS.Green( obj.prefix ) , obj.CS.Cyan( err ) ) )
+}
---
+++ lib/logs/logs_test.go
@@ -0,0 +1,36 @@
+package logs
+
+import (
+	"testing"
+)
+
+func TestNewLogger(t *testing.T) {
+	var log = NewLogger("main")
+	if log == nil {
+		t.Error("Cannot create logger")
+	}
+}
+
+func Test_NewLoggerJSON(t *testing.T) {
+
+	// as we are exiting, revert sqlOpen back to oldSqlOpen at end of function
+	defer func() { OsGetenv = oldosGetenv }()
+	var newVal = func(key string) string {
+		return "JSON"
+	}
+	OsGetenv = newVal
+
+	var log = NewLogger("main")
+	if log == nil {
+		t.Error("Cannot create logger")
+	}
+
+}
+
+func Test_SubLogger(t *testing.T) {
+	var log = NewLogger("main")
+	var log1 = log.SubLogger("subLogger")
+	if log1 == nil {
+		t.Error("Cannot create logger")
+	}
+}
